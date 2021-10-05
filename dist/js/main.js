6ca46464cd18019a26edb47f54605f4182ac50e2
Rebecca Tracey-Timoney <71783662+rebeccatraceyt@users.noreply.github.com>
2021-10-05 19:31:08 +0100

    Add large navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,6 +17,42 @@ $(function(){
         $('#socials span').not(':last-child').toggleClass('hidden show');
     });
 
+    // Navbar active class
+    let menuScrollTimer = null;
+    $(".nav-active a").click(function (e) {
+        // Prevent default behaviour ( scroll to element )
+        e.preventDefault();
+        if (menuScrollTimer === null) {
+            // Highlight the clicked item
+            $('.nav-active a.active').removeClass('active');
+            $(this).addClass('active');
+            // Smooth scroll to the target section
+            let target = $(this).attr('href');
+            $('html, body').animate({ scrollTop: $(target).offset().top - 100 }, 1050);
+            // Set `menuScrollTimer` timer
+            // This will prevents multiple clicks on menu items whule the smooth scroll is taking effect
+            // This will also prevent the scroll logic from running
+            menuScrollTimer = setTimeout(function () {
+                clearTimeout(menuScrollTimer);
+                menuScrollTimer = null;
+            }, 1050);
+        }
+    });
+
+    $(window).scroll(function (e) {
+        // Avoid triggering the logic if the scroll event is triggered from clicking one of the items
+        if (menuScrollTimer === null) {
+            let windowTop = $(this).scrollTop();
+
+            $('.nav-active a').each(function (event) {
+                if (windowTop >= $($(this).attr('href')).offset().top - 100) {
+                    $('.nav-active .active').removeClass('active');
+                    $(this).addClass('active');
+                }
+            });
+        }
+    });
+
     // Footer Scroll function
     $(window).scroll(function() {
         if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
